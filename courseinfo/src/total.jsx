027e78bc5528e8ba0d9cc6df90680dc421d7684c
DarboeDev <darboedev@gmail.com>
2024-03-04 14:22:50 +0000

    solution 1

---
+++ courseinfo/src/total.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+const Total = (props)=>{
+    return(
+        <>
+        <p>Number of exersises {props.total}</p>
+        </>
+    )
+};
+
+
+export default Total;
